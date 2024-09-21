b9dc5865f48d75fcd9b652abacf0d31ea56cd613
Hacker-tech-lang <compasivochristianpaul22@gmail.com>
2024-09-21 22:08:00 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('stream', (data) => {
+        socket.broadcast.emit('stream', data);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
